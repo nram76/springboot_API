b214fee812a2bc2dfeac950382500995b624e917
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2024-01-08 13:53:50 -0600

    initial commit

---
+++ src/main/java/dev/nav/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.nav.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}

80ae63d4867cb3d8752dcef1c95e2f080aba313f
Naveen Ramanjulu <55444343+nram76@users.noreply.github.com>
2024-01-10 21:39:31 -0600

    added cors policy for client app

--- src/main/java/dev/nav/movies/MovieController.java
+++ src/main/java/dev/nav/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,11 +16,13 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<List<Movie>> allMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies() , HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping("/{imdbId}")
     public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String imdbId) {
         return new ResponseEntity<Optional<Movie>>( movieService.singleMovie(imdbId), HttpStatus.OK);
